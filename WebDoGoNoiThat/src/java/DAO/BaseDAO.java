1780cb76f976b3923368ca1e721f59b798ba72e2
tuanvuongquoc27 <tienvuong323@gmail.com>
2022-02-23 02:02:36 +0700

    connect database
    connect database with project

---
+++ WebDoGoNoiThat/src/java/DAO/BaseDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class BaseDAO {
+
+    public static Connection open() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection("jdbc:ssqlserver://localhost:1433;databaseName=DoGoNoiThat", "sa", "123456");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(BaseDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+}
